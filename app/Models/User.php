64e6c4b9f2c285b84a8cb5952668252fad4af294
MEBAKHA <koboycilok@gmail.com>
2026-04-17 10:39:34 +0700

    creating API

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,21 +3,22 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Foundation\Auth\User as Authenticatable;
+    use Illuminate\Notifications\Notifiable;
+    use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
-{
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    class User extends Authenticatable
+    {
+        /** @use HasFactory<\Database\Factories\UserFactory> */
+        use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $guarded = ['id'];
+        /**
+         * The attributes that are mass assignable.
+         *
+         * @var list<string>
+         */
+        protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
